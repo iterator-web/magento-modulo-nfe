af48bcf13152ce06bc96ec22905bc4e70d5f9fb3
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2014-10-29 15:39:05 -0200

    Commit referente ao issue #1
    Estruturação do Módulo

---
+++ app/code/local/Iterator/Nfe/sql/nfe_setup/mysql4-install-1.0.0.php
@@ -0,0 +1,59 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                     MÓDULO DE INTEGRAÇÃO NF-E                          
+ * =================================================================
+ * Este produto foi desenvolvido para integrar o Ecommerce Magento
+ * ao Sistema da SEFAZ para geração de Nota Fiscal Eletrônica(NF-e).
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a gerar o XML da NF-e, validar e assinar digitalmente em
+ * ambiente da própria loja virtual. Também terá a possibilidade de 
+ * fazer outros processos diretos com o SEFAZ como cancelamentos de
+ * NF-e, consultas e inutilizações de numeração. O módulo faz ainda
+ * o processo de geração da DANFE e envio automático de e-mail ao
+ * cliente com as informações e arquivos relacionados a sua NF-e.
+ * Por fim o módulo disponibiliza também a NF-e de entrada que será
+ * gerada no momento da devolução de pedidos por parte dos clientes.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_Nfe
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+$installer = $this;
+$installer->startSetup();
+$installer->run("
+  CREATE  TABLE IF NOT EXISTS `{$installer->getTable('nfe/nfe')}` (
+    `nfe_id` INT(12) UNSIGNED NOT NULL AUTO_INCREMENT ,
+    `numero_recibo` INT(20) UNSIGNED ,
+    `pedido_increment_id` varchar(18) NOT NULL ,
+    `numero_nfe` INT(20) UNSIGNED ,
+    `caminho_arquivos` varchar(60) NOT NULL ,
+    `status` TINYINT(1) NOT NULL ,
+    `mensagem` VARCHAR(255) NULL ,
+    `data_nfe` datetime ,
+    `data_autorizacao` datetime,
+    PRIMARY KEY (`nfe_id`) )
+  ENGINE = InnoDB DEFAULT CHARSET=utf8;
+  ");
+  
+$status = Mage::getModel('sales/order_status');
+$status->setStatus('nfe_aguardando')->setLabel(utf8_encode('Aguardando Autorização NF-e'))
+    ->assignState(Mage_Sales_Model_Order::STATE_PROCESSING)
+    ->save();
+
+$installer->endSetup();
+
+?>
