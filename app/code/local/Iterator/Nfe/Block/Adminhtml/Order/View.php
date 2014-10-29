cdbf6983e89275aa7a747df39bc671eff9e49372
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2014-10-29 18:40:49 -0200

    Commit referente ao issue #3
    Gerar NF-e por pedido

---
+++ app/code/local/Iterator/Nfe/Block/Adminhtml/Order/View.php
@@ -0,0 +1,53 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                     MÓDULO DE INTEGRAÇÃO NF-E                          
+ * =================================================================
+ * Este produto foi desenvolvido para integrar o Ecommerce Magento
+ * ao Sistema da SEFAZ para geração de Nota Fiscal Eletrônica(NF-e).
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a gerar o XML da NF-e, validar e assinar digitalmente em
+ * ambiente da própria loja virtual. Também terá a possibilidade de 
+ * fazer outros processos diretos com o SEFAZ como cancelamentos de
+ * NF-e, consultas e inutilizações de numeração. O módulo faz ainda
+ * o processo de geração da DANFE e envio automático de e-mail ao
+ * cliente com as informações e arquivos relacionados a sua NF-e.
+ * Por fim o módulo disponibiliza também a NF-e de entrada que será
+ * gerada no momento da devolução de pedidos por parte dos clientes.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_Nfe
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_Nfe_Block_Adminhtml_Order_View extends Mage_Adminhtml_Block_Sales_Order_View {
+    
+    public function  __construct()  {
+        parent::__construct();
+        $status = $this->getOrder()->getStatus();
+        
+        if($status == 'processing' || $status == 'nfe_cancelada') {
+            $this->_addButton('gerar_nfe', array(
+                    'label'     => 'Gerar NF-e',
+                    'onclick'   => 'setLocation(\'' . $this->getUrl('*/nfe/gerarNfe') . '\')',
+                    'class'     => 'go'
+            ));
+            $this->_removeButton('order_ship');
+            $this->_removeButton('order_hold');
+        } else {
+            return;
+        }
+    }
+}
